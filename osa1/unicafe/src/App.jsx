1d49856f9aa306a598a54c737d644acd7194be8d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-23 19:28:30 +0200

    Lisää tapahtumankäsittelijät

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -10,13 +10,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleClick = (counter, setCounter) => {
+    const handler = () => {
+      console.log("counter before:", counter);
+      let updatedCounter = counter + 1;
+      console.log("counter after:", updatedCounter);
+      setCounter(updatedCounter);
+    };
+
+    return handler;
+  };
+
   return (
     <div>
       <h1>Give feedback</h1>
 
-      <Button handleClick={setGood} text="good"></Button>
-      <Button handleClick={setNeutral} text="neutral"></Button>
-      <Button handleClick={setBad} text="bad"></Button>
+      <Button handleClick={handleClick(good, setGood)} text="good"></Button>
+      <Button
+        handleClick={handleClick(neutral, setNeutral)}
+        text="neutral"
+      ></Button>
+      <Button handleClick={handleClick(bad, setBad)} text="bad"></Button>
     </div>
   );
 };
