25e661db306d216d5105e8a6b8b48f395cb232aa
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-23 19:00:19 +0200

    Lisää napit ja otsikko

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>text</button>;
+  return <button onClick={handleClick}>{text}</button>;
 };
 
 const App = () => {
@@ -10,7 +10,15 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <Button handleClick={setGood} text="good"></Button>
+      <Button handleClick={setNeutral} text="neutral"></Button>
+      <Button handleClick={setBad} text="bad"></Button>
+    </div>
+  );
 };
 
 export default App;
