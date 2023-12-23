8536495f5dbdd7646ce111e62788f2ecc499535b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-23 19:48:36 +0200

    Näytä palautteita ainoastaan, jos palautteita on annettu

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -17,15 +17,26 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
   const positive = good / total;
 
+  let content = <>No feedback given</>;
+
+  if (total > 0) {
+    content = (
+      <>
+        {" "}
+        <Status text="good" value={good}></Status>
+        <Status text="neutral" value={neutral}></Status>
+        <Status text="bad" value={bad}></Status>
+        <p>all {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive}</p>
+      </>
+    );
+  }
+
   return (
     <div>
       <h1>Statistics</h1>
-      <Status text="good" value={good}></Status>
-      <Status text="neutral" value={neutral}></Status>
-      <Status text="bad" value={bad}></Status>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      {content}
     </div>
   );
 };
