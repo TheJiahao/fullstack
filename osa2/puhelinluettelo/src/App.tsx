cd17254efb33a00b75cce25e1e6759e001fe06cc
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-24 16:24:12 +0200

    Lisää testidataa

--- osa2/puhelinluettelo/src/App.tsx
+++ osa2/puhelinluettelo/src/App.tsx
@@ -5,6 +5,9 @@ import FilterForm from "./components/FilterForm";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewPhoneNumber] = useState("");
