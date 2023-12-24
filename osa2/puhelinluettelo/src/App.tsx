89606ba33582881ab61f3dacdcb57bc4e347c922
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-24 15:32:54 +0200

    Lisää t2.6

--- osa2/puhelinluettelo/src/App.tsx
+++ osa2/puhelinluettelo/src/App.tsx
@@ -1,22 +1,35 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import PersonList from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addNumber = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("button clicked");
+
+    setPersons(persons.concat({ name: newName }));
+  };
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNumber}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <PersonList persons={persons} />
     </div>
   );
 };
