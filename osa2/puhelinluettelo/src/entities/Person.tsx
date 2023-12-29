306d8745a24935d9a9fb772743812433cd410b85
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-29 13:22:54 +0200

    Lisää oletus id Person-luokalle

--- osa2/puhelinluettelo/src/entities/Person.tsx
+++ osa2/puhelinluettelo/src/entities/Person.tsx
@@ -1,9 +1,10 @@
 class Person {
-  id: number = -1;
+  id: number;
   name: string;
   number: string;
 
-  constructor(name: string, number: string) {
+  constructor(name: string, number: string, id: number = -1) {
+    this.id = id;
     this.name = name;
     this.number = number;
   }
