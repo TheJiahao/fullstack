f314641d37f2002597f1103553084d316a76d510
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-31 11:17:15 +0200

    Refaktoroi yhden maan tiedon näyttäminen omaksi komponentiksi

---
+++ osa2/maidenTiedot/src/components/CountryEntry.tsx
@@ -0,0 +1,26 @@
+import Country from "../interfaces/Country";
+
+const CountryEntry = ({ country }: { country: Country }) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+
+      <p>
+        capital {country.capital}
+        <br />
+        area {country.area}
+      </p>
+
+      <h3>languages</h3>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+
+      <img src={country.flags.svg} alt={country.flags.alt} width="10%"></img>
+    </div>
+  );
+};
+
+export default CountryEntry;
--- osa2/maidenTiedot/src/components/CountryList.tsx
+++ osa2/maidenTiedot/src/components/CountryList.tsx
@@ -1,37 +1,12 @@
 import Country from "../interfaces/Country";
+import CountryEntry from "./CountryEntry";
 
 const CountryList = ({ countries }: { countries: Country[] }) => {
   const n = countries.length;
 
   switch (true) {
-    case n === 1: {
-      const country = countries[0];
-
-      return (
-        <div>
-          <h2>{country.name.common}</h2>
-
-          <p>
-            capital {country.capital}
-            <br />
-            area {country.area}
-          </p>
-
-          <h3>languages</h3>
-          <ul>
-            {Object.values(country.languages).map((language) => (
-              <li key={language}>{language}</li>
-            ))}
-          </ul>
-
-          <img
-            src={country.flags.svg}
-            alt={country.flags.alt}
-            width="10%"
-          ></img>
-        </div>
-      );
-    }
+    case n === 1:
+      return <CountryEntry country={countries[0]} />;
     case n <= 10:
       return (
         <div>
