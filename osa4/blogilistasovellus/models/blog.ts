a3c6931682b22d36c647c8d8971dc48bb8710cdc
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-05 12:31:36 +0200

    t4.2: Siirrä blogin skeema omaan moduuliin

---
+++ osa4/blogilistasovellus/models/blog.ts
@@ -0,0 +1,18 @@
+import mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export = mongoose.model("Blog", blogSchema);
