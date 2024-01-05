11718e60432335bab5e0815341f8efc61092cfc6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-05 14:43:23 +0200

    t4.5: Lisää favoriteBlog

--- osa4/blogilistasovellus/utils/list_helper.ts
+++ osa4/blogilistasovellus/utils/list_helper.ts
@@ -8,4 +8,12 @@ const totalLikes = (blogs: Blog[]): number => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-export default { dummy, totalLikes };
+const favoriteBlog = (blogs: Blog[]): Blog | null => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
+};
+
+export default { dummy, totalLikes, favoriteBlog };
