6c7c32728a867a85fb00fd89b8ba98ad45960717
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-07 11:36:40 +0200

    t4.8: Lisää test_helper

---
+++ osa4/blogilistasovellus/tests/test_helper.ts
@@ -0,0 +1,18 @@
+import Blog from "../interfaces/blog";
+
+const initialBlogs: Blog[] = [
+  {
+    author: "Jaska Jokunen",
+    title: "How to make a fusion power plant at home",
+    url: "https://blog.test",
+    likes: 1000000,
+  },
+  {
+    author: "Blogger",
+    title: "TypeScript is better than JavaScript",
+    url: "https://blog.test/typescript",
+    likes: 2,
+  },
+];
+
+export default { initialBlogs };
