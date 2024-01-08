5cf91f9b827a40f1ef0253881f0b877947110ef7
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-08 15:08:31 +0200

    t4.16: Korjaa users/helper

--- osa4/blogilistasovellus/tests/controllers/users/helper.ts
+++ osa4/blogilistasovellus/tests/controllers/users/helper.ts
@@ -1,6 +1,6 @@
 const initialUsers = [
   { username: "user1", name: "Etunimi Sukunimi", password: "1234" },
-  { username: "user1", name: "Nimi", password: "123456" },
+  { username: "user2", name: "Nimi", password: "123456" },
 ];
 
 const baseRoute = "/api/users";
