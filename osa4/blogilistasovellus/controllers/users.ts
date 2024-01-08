259deb0b92f3eefce57b1f4edc8f3d598c8477be
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-08 14:37:45 +0200

    t4.15: Lisää käyttäjien hakupyyntö

--- osa4/blogilistasovellus/controllers/users.ts
+++ osa4/blogilistasovellus/controllers/users.ts
@@ -16,4 +16,10 @@ userRouter.post("/", async (request, response) => {
   response.status(201).json(savedUser);
 });
 
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+
+  response.status(200).json(users);
+});
+
 export default userRouter;
