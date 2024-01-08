a22c9188ffde13cd67a1251a91d56311ada57d34
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-08 14:24:23 +0200

    t4.15: Lisää testit käyttäjän lisäykselle

---
+++ osa4/blogilistasovellus/tests/controllers/users/add_user.test.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import supertest from "supertest";
+import app from "../../../app";
+import User from "../../../models/user";
+import helper from "./helper";
+
+const api = supertest(app);
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("adding user", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    await User.insertMany(helper.initialUsers);
+  });
+
+  test("succeeds with a valid user", async () => {
+    const user = {
+      username: "cooluser",
+      name: "Cool User",
+      password: "aiosjaiasds12312o",
+    };
+
+    await api
+      .post("/api/users")
+      .send(user)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  });
+});
