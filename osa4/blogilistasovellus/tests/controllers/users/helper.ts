07b2ffecb4fd67f8659afc4f15fe11980c35b980
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-08 13:53:16 +0200

    t4.15: Lisää helper blogin API:n testeille

---
+++ osa4/blogilistasovellus/tests/controllers/users/helper.ts
@@ -0,0 +1,6 @@
+const getInitialUsers = [
+  { username: "user1", name: "Etunimi Sukunimi", password: "1234" },
+  { username: "user1", name: "Nimi", password: "123456" },
+];
+
+export default { getInitialUsers };
