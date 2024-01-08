7a30458f23437c34fc9e4266764ecb019efb4916
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-08 12:59:45 +0200

    t4.15: Lisää model käyttäjälle

---
+++ osa4/blogilistasovellus/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+export default mongoose.model("User", userSchema);
