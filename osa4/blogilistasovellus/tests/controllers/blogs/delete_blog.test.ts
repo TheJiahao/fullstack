0cd55871b33ba1cacb8cd3037c3d5720e28c0f9d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-09 16:31:32 +0200

    t4.23: Korjaa blogin poiston testit

--- osa4/blogilistasovellus/tests/controllers/blogs/delete_blog.test.ts
+++ osa4/blogilistasovellus/tests/controllers/blogs/delete_blog.test.ts
@@ -3,6 +3,9 @@ import supertest from "supertest";
 import app from "../../../app";
 import Blog from "../../../models/blog";
 import blogHelper from "../blog_helper";
+import userHelper from "../user_helper";
+import loginHelper from "../login_helper";
+import User from "../../../models/user";
 
 const api = supertest(app);
 
@@ -12,13 +15,28 @@ afterAll(async () => {
 
 describe("deletion of blogs", () => {
   beforeEach(async () => {
+    await User.deleteMany({});
+    await userHelper.addInitialUsers(api);
     await Blog.deleteMany({});
-    await Blog.insertMany(blogHelper.initialBlogs);
+
+    const { username } = userHelper.initialUsers[0];
+    const userId = (await User.findOne({ username })).id;
+
+    await Blog.insertMany(
+      blogHelper.initialBlogs.map((blog) => {
+        return { ...blog, user: userId };
+      })
+    );
   });
 
-  test("succeeds when deleting existing blog", async () => {
+  test("succeeds when deleting existing blog with valid token", async () => {
+    const token = await loginHelper.getToken(api, userHelper.initialUsers[0]);
+
     const blogToDelete = (await blogHelper.getAllBlogs())[0];
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(204);
 
     const blogs = await blogHelper.getAllBlogs();
 
@@ -26,9 +44,13 @@ describe("deletion of blogs", () => {
   });
 
   test("succeeds when deleting non-existent blog", async () => {
+    const token = await loginHelper.getToken(api, userHelper.initialUsers[0]);
     const id = await blogHelper.getNonExistingBlogId();
 
-    await api.delete(`/api/blogs/${id}`).expect(204);
+    await api
+      .delete(`/api/blogs/${id}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(204);
 
     const blogs = await blogHelper.getAllBlogs();
     expect(blogs).toHaveLength(blogHelper.initialBlogs.length);
