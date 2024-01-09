9b7c9c50c926714b4b0d4a2d88fc3a2f6446e425
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-09 15:45:24 +0200

    t4.23: Erottele helperit

--- osa4/blogilistasovellus/tests/controllers/blogs/delete_blog.test.ts
+++ osa4/blogilistasovellus/tests/controllers/blogs/delete_blog.test.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import supertest from "supertest";
 import app from "../../../app";
 import Blog from "../../../models/blog";
-import helper from "../blog_helper";
+import blogHelper from "../blog_helper";
 
 const api = supertest(app);
 
@@ -13,24 +13,24 @@ afterAll(async () => {
 describe("deletion of blogs", () => {
   beforeEach(async () => {
     await Blog.deleteMany({});
-    await Blog.insertMany(helper.initialBlogs);
+    await Blog.insertMany(blogHelper.initialBlogs);
   });
 
   test("succeeds when deleting existing blog", async () => {
-    const blogToDelete = (await helper.getAllBlogs())[0];
+    const blogToDelete = (await blogHelper.getAllBlogs())[0];
     await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-    const blogs = await helper.getAllBlogs();
+    const blogs = await blogHelper.getAllBlogs();
 
-    expect(blogs).toHaveLength(helper.initialBlogs.length - 1);
+    expect(blogs).toHaveLength(blogHelper.initialBlogs.length - 1);
   });
 
   test("succeeds when deleting non-existent blog", async () => {
-    const id = await helper.getNonExistingBlogId();
+    const id = await blogHelper.getNonExistingBlogId();
 
     await api.delete(`/api/blogs/${id}`).expect(204);
 
-    const blogs = await helper.getAllBlogs();
-    expect(blogs).toHaveLength(helper.initialBlogs.length);
+    const blogs = await blogHelper.getAllBlogs();
+    expect(blogs).toHaveLength(blogHelper.initialBlogs.length);
   });
 });
