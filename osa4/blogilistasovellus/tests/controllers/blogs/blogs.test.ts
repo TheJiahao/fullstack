c820b34b8dc10517b5520d5120a4d4375ffb60d6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-09 15:26:36 +0200

    t4.23: Siirrä blogien poiston testit omaan moduuliin

--- osa4/blogilistasovellus/tests/controllers/blogs/blogs.test.ts
+++ osa4/blogilistasovellus/tests/controllers/blogs/blogs.test.ts
@@ -10,31 +10,6 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("deletion of blogs", () => {
-  beforeEach(async () => {
-    await blog.deleteMany({});
-    await blog.insertMany(helper.initialBlogs);
-  });
-
-  test("succeeds when deleting existing blog", async () => {
-    const blogToDelete = (await helper.getAllBlogs())[0];
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-    const blogs = await helper.getAllBlogs();
-
-    expect(blogs).toHaveLength(helper.initialBlogs.length - 1);
-  });
-
-  test("succeeds when deleting non-existent blog", async () => {
-    const id = await helper.getNonExistingBlogId();
-
-    await api.delete(`/api/blogs/${id}`).expect(204);
-
-    const blogs = await helper.getAllBlogs();
-    expect(blogs).toHaveLength(helper.initialBlogs.length);
-  });
-});
-
 describe("updating blogs", () => {
   beforeEach(async () => {
     await blog.deleteMany({});
