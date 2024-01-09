fd37ff9c32f834db0c05e3ece9ed79670bf61d10
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-09 11:02:31 +0200

    t4.17: Näytä käyttäjän lisäämät blogit listauksessa

--- osa4/blogilistasovellus/controllers/users.ts
+++ osa4/blogilistasovellus/controllers/users.ts
@@ -26,7 +26,11 @@ userRouter.post("/", async (request, response) => {
 });
 
 userRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+  });
 
   response.status(200).json(users);
 });
