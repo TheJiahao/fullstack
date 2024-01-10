9fd34055d96b5f4f2ae011cbff66dfcd2737f167
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-10 13:25:06 +0200

    t5.1: Lisää tyhjä kirjautumislomake

--- osa5/bloglist-frontend/src/App.tsx
+++ osa5/bloglist-frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from "react";
 import Blog, { BlogProps } from "./components/Blog";
+import LoginForm from "./components/LoginForm";
 import blogService from "./services/blogs";
+
+interface User {
+  username: string;
+  password: string;
+  token?: string;
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState<BlogProps[]>([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -11,6 +21,14 @@ const App = () => {
 
   return (
     <div>
+      <LoginForm
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+        handleLogin={() => {}}
+      />
+
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
