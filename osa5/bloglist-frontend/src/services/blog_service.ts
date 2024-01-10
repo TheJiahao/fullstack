3cfb218d352475b1b92af76e0ea9aec9bb7ca9c4
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-10 14:58:47 +0200

    t5.3: Lisää blogServiceen blogien lisäys ja tokenin asetus

--- osa5/bloglist-frontend/src/services/blog_service.ts
+++ osa5/bloglist-frontend/src/services/blog_service.ts
@@ -1,9 +1,32 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { BlogProps } from "../components/Blog";
 const baseUrl = "/api/blogs";
+
+let token: string | null = null;
+
+const setToken = (newToken: string): void => {
+  token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
-export default { getAll };
+const create = async (
+  title: string,
+  author: string,
+  url: string
+): Promise<BlogProps> => {
+  const config: AxiosRequestConfig = {
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  const response = await axios.post(baseUrl, { title, author, url }, config);
+
+  return response.data;
+};
+
+export default { setToken, getAll, create };
