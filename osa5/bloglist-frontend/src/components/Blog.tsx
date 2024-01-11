92f1ff622c8ff9452ad7655a128820a9ce6b1d12
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-11 13:25:15 +0200

    t5.7: Lisää nappi blogin tietojen näyttämiseen

--- osa5/bloglist-frontend/src/components/Blog.tsx
+++ osa5/bloglist-frontend/src/components/Blog.tsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
+import User from "../interfaces/user";
+
 interface BlogProps {
   id: string;
   title: string;
   author: string;
   url: string;
   likes: number;
+  user: User;
 }
 
-const Blog = ({ blog }: { blog: BlogProps }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }: { blog: BlogProps }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+  const buttonLabel = visible ? "hide" : "view";
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setVisible(!visible)}>{buttonLabel}</button>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a>
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export type { BlogProps };
 export default Blog;
