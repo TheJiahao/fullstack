83fba73fda3cd108a99a0f19f5ff184ec21a5d72
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-13 13:38:47 +0200

    t5.17: Konfiguroi cypress

--- osa5/bloglist-frontend/.eslintrc.cjs
+++ osa5/bloglist-frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "cypress/globals": true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -9,7 +9,7 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "jest"],
+  plugins: ["react-refresh", "jest", "cypress"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
