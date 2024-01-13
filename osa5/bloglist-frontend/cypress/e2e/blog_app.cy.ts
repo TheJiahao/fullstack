6894db5c841ec75c5efdc8093b2515f382403044
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-13 14:26:14 +0200

    t5.18: Lisää testit kirjautumiselle

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.ts
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.ts
@@ -1,6 +1,14 @@
 describe("Blog app", function () {
+  const user = {
+    username: "testuser",
+    name: "Test Tset",
+    password: "PassWord",
+  };
+
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", "http://localhost:3003/api/users", user);
+
     cy.visit("http://localhost:5173");
   });
 
@@ -8,5 +16,23 @@ describe("Blog app", function () {
     cy.get("#username-input");
     cy.get("#password-input");
     cy.get("#login-button");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username-input").type(user.username);
+      cy.get("#password-input").type(user.password);
+      cy.get("#login-button").click();
+
+      cy.contains(`${user.name} logged in`);
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username-input").type("wrongusername");
+      cy.get("#password-input").type("wrongPassword");
+      cy.get("#login-button").click();
+
+      cy.contains(`Invalid credentials`);
+    });
   });
 });
