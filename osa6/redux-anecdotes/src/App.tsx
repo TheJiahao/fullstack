034651c2873ddf837193ba9023756d4cf0667d6b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-10 17:13:54 +0200

    t6.14: Hae anekdootit backendistä

--- osa6/redux-anecdotes/src/App.tsx
+++ osa6/redux-anecdotes/src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import anecdoteService from "./services/anecdoteService";
+import store from "./store";
+
+import { setAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
+    useEffect(() => {
+        anecdoteService
+            .getAllAnecdotes()
+            .then((anecdotes) => store.dispatch(setAnecdotes(anecdotes)));
+    }, []);
+
     return (
         <div>
             <h2>Anecdotes</h2>
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.ts
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.ts
@@ -1,15 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { AnecdoteProps } from "../components/Anecdote";
 
-const anecdotesAtStart = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote: string): AnecdoteProps => {
@@ -20,11 +11,9 @@ const asObject = (anecdote: string): AnecdoteProps => {
     };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
-
 const anecdoteSlice = createSlice({
     name: "anecdotes",
-    initialState,
+    initialState: new Array<AnecdoteProps>(),
     reducers: {
         voteAnecdote(state: AnecdoteProps[], action: PayloadAction<string>) {
             console.log("state now: ", state);
@@ -55,8 +44,12 @@ const anecdoteSlice = createSlice({
             newState = state.concat(asObject(anecdote));
             return newState.sort((a, b) => b.votes - a.votes);
         },
+        setAnecdotes(state, action: PayloadAction<AnecdoteProps[]>) {
+            return action.payload;
+        },
     },
 });
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions;
+export const { createAnecdote, voteAnecdote, setAnecdotes } =
+    anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
