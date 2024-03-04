0bf17e2aaeb0052d5f6c49bb741740c7f643efb9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-04 09:30:49 +0200

    t7.6: Korjaa Invalid value for prop 'reset' on <input> tag

--- osa7/routed-anecdotes/src/components/CreateNew.tsx
+++ osa7/routed-anecdotes/src/components/CreateNew.tsx
@@ -1,9 +1,9 @@
 import useField from "../hooks/index";
 
 const CreateNew = (props) => {
-    const content = useField("text");
-    const author = useField("text");
-    const info = useField("text");
+    const { reset: resetContent, ...content } = useField("text");
+    const { reset: resetAuthor, ...author } = useField("text");
+    const { reset: resetInfo, ...info } = useField("text");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,9 +16,9 @@ const CreateNew = (props) => {
     };
 
     const handleReset = () => {
-        content.reset();
-        author.reset();
-        info.reset();
+        resetContent();
+        resetAuthor();
+        resetInfo();
     };
 
     return (
