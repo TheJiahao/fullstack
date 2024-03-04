c8cce1586fc901c8da2a9f51992dae07a127431c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-04 10:19:09 +0200

    t7.7: Toteuta useCountry

--- osa7/country-hook/src/App.jsx
+++ osa7/country-hook/src/App.jsx
@@ -18,7 +18,30 @@ const useField = (type) => {
 const useCountry = (name) => {
     const [country, setCountry] = useState(null);
 
-    useEffect(() => {});
+    useEffect(() => {
+        const newCountry = axios
+            .get(
+                `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+            )
+            .then((response) => {
+                const data = {
+                    name: response.data.name.common,
+                    capital: response.data.capital[0],
+                    population: response.data.population,
+                    flag: response.data.flags.svg,
+                };
+
+                setCountry({
+                    found: true,
+                    data,
+                });
+            })
+            .catch(() => {
+                setCountry({
+                    found: false,
+                });
+            });
+    }, [name]);
 
     return country;
 };
