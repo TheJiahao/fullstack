498bcc58675ccb7ba48e3ada124bc4c3a039517b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 11:43:17 +0200

    t7.14: Lisää vapaaehtoinen property blogs

--- osa7/bloglist-frontend/src/interfaces/user.ts
+++ osa7/bloglist-frontend/src/interfaces/user.ts
@@ -1,8 +1,11 @@
+import { BlogProps } from "../components/Blog";
+
 interface User {
     username: string;
     name: string;
     password?: string;
     token: string;
+    blogs?: BlogProps[];
 }
 
 export default User;
