80eb3ae0eb31c55d4d36c3f0fc88f129099a8fd0
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 09:47:50 +0200

    t7.13: Lisää AppThunk

--- osa7/bloglist-frontend/src/store.ts
+++ osa7/bloglist-frontend/src/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { ThunkAction, UnknownAction, configureStore } from "@reduxjs/toolkit";
 import notificationSlice from "./reducers/notificationReducer";
 import blogReducer from "./reducers/blogReducer";
 
@@ -11,3 +11,9 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    UnknownAction
+>;
