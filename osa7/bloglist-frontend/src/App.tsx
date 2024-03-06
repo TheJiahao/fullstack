357313fe65c468c398ad14adbd9b3d465b5ca7ee
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 17:50:40 +0200

    t7.20: Älä näytä navigaatiomenua ennen kirjautumista

--- osa7/bloglist-frontend/src/App.tsx
+++ osa7/bloglist-frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
 
     return (
         <div className="container">
-            <NavigationBar />
+            {loggedUser && <NavigationBar />}
             <Notification />
 
             {loggedUser ? (
