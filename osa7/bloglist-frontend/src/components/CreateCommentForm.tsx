a6154d311232a3798510e2fa00a71842f71bc05c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 16:59:52 +0200

    t7.19: Toteuta kommentointi frontendissä

--- osa7/bloglist-frontend/src/components/CreateCommentForm.tsx
+++ osa7/bloglist-frontend/src/components/CreateCommentForm.tsx
@@ -1,24 +1,36 @@
+import { FormEvent } from "react";
 import { useMatch } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import useField from "../hooks/useField";
-import { useAppSelector } from "../hooks";
+import { commentBlog } from "../reducers/blogReducer";
 import logger from "../utils/logger";
 
 const CreateCommentForm = () => {
     const match = useMatch("/blogs/:id");
-    const comment = useField("text");
+    const dispatch = useAppDispatch();
+
+    const { reset: resetComment, ...comment } = useField("text");
     const blogs = useAppSelector((state) => state.blogs);
 
-    const blogId = match
+    const blog = match
         ? blogs.find((blog) => blog.id === match.params.id)
         : null;
 
-    if (!blogId) {
+    if (!blog) {
         logger.error("CreateCommentForm should be inside Blog component");
         return null;
     }
 
+    const handleComment = (event: FormEvent) => {
+        event.preventDefault();
+
+        dispatch(commentBlog({ blogId: blog.id, content: comment.value }));
+
+        resetComment();
+    };
+
     return (
-        <form>
+        <form onSubmit={handleComment}>
             <input {...comment} />
             <button type="submit">add comment</button>
         </form>
