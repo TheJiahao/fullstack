21b310b3dd1c7d677f8cb04246fcc09ea80be171
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 17:14:29 +0200

    t7.20: Muuta BlogList käyttämään Bootstrapia

--- osa7/bloglist-frontend/src/components/BlogList.tsx
+++ osa7/bloglist-frontend/src/components/BlogList.tsx
@@ -1,27 +1,24 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../hooks";
+import { Table } from "react-bootstrap";
 
 const BlogList = () => {
     const blogs = useAppSelector((state) => state.blogs);
 
-    const blogStyle = {
-        paddingTop: 10,
-        paddingLeft: 2,
-        border: "solid",
-        borderWidth: 1,
-        marginBottom: 5,
-    };
-
     return (
-        <div>
-            {blogs.map((blog) => (
-                <div key={blog.id} className="blog" style={blogStyle}>
-                    <Link to={`/blogs/${blog.id}`}>
-                        {blog.title} {blog.author}
-                    </Link>
-                </div>
-            ))}
-        </div>
+        <Table striped bordered hover>
+            <tbody>
+                {blogs.map((blog) => (
+                    <tr key={blog.id} className="blog">
+                        <td>
+                            <Link to={`/blogs/${blog.id}`}>
+                                {blog.title} {blog.author}
+                            </Link>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     );
 };
 
