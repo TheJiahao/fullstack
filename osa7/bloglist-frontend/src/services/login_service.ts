e04abf3179ceec2f17ffcce472e790b7e6d3119e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 10:15:39 +0200

    t7.13: Lisää tyyppi funktiolle login_service.login

--- osa7/bloglist-frontend/src/services/login_service.ts
+++ osa7/bloglist-frontend/src/services/login_service.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import User from "../interfaces/user";
 const baseUrl = "/api/login";
 
-const login = async (username: string, password: string) => {
+const login = async (username: string, password: string): Promise<User> => {
     const response = await axios.post(baseUrl, { username, password });
     return response.data;
 };
