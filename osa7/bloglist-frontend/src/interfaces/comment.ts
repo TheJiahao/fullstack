4c593f60ffcf13b2a3217ba9781b423ee47724a2
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 16:04:35 +0200

    t7.19: Päivitä kommentin tyyppi

--- osa7/bloglist-frontend/src/interfaces/comment.ts
+++ osa7/bloglist-frontend/src/interfaces/comment.ts
@@ -1,6 +1,7 @@
 interface Comment {
     id: string;
     content: string;
+    blog: { id: string };
 }
 
 export default Comment;
--- osa7/bloglist-frontend/src/services/commentService.ts
+++ osa7/bloglist-frontend/src/services/commentService.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import Comment from "../interfaces/comment";
+
 const baseUrl = "/api/blogs";
 
-const create = async (id: string, content: string) => {
+const create = async (id: string, content: string): Promise<Comment> => {
     const response = await axios.post(`${baseUrl}/${id}/comments`, { content });
     return response.data;
 };
