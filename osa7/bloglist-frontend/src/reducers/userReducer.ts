41bbce8fc19e9d149254b5a842bd69523ac28213
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 10:16:14 +0200

    t7.13: Lisää login ja logout actionit

--- osa7/bloglist-frontend/src/reducers/userReducer.ts
+++ osa7/bloglist-frontend/src/reducers/userReducer.ts
@@ -1,6 +1,8 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import User from "../interfaces/user";
 import blogService from "../services/blog_service";
+import loginService from "../services/login_service";
+import { setNotification } from "./notificationReducer";
 
 const initializeUser = (): User | null => {
     const loggedUserJSON = window.localStorage.getItem("loggedUser");
@@ -15,6 +17,27 @@ const initializeUser = (): User | null => {
     return user;
 };
 
+const login = createAsyncThunk(
+    "user/login",
+    async (
+        { username, password }: { username: string; password: string },
+        thunkAPI,
+    ) => {
+        try {
+            const user = await loginService.login(username, password);
+
+            window.localStorage.setItem("loggedUser", JSON.stringify(user));
+            blogService.setToken(user.token);
+
+            thunkAPI.dispatch(setNotification("Logged in"));
+
+            return user;
+        } catch (error) {
+            thunkAPI.dispatch(setNotification("Invalid credentials"));
+        }
+    },
+);
+
 const userSlice = createSlice({
     name: "user",
     initialState: initializeUser,
@@ -22,8 +45,22 @@ const userSlice = createSlice({
         setUser(state, action: PayloadAction<User>) {
             return action.payload;
         },
+        logout(state) {
+            return null;
+        },
     },
+    extraReducers: (builder) =>
+        builder.addCase(login.fulfilled, (state, action) => {
+            const user = action.payload;
+
+            if (!user) {
+                return null;
+            }
+
+            return user;
+        }),
 });
 
 export default userSlice.reducer;
-export const { setUser } = userSlice.actions;
+export const { setUser, logout } = userSlice.actions;
+export { login };
