19c16f384d8c37a41a793fbec7d01b15bf0eaa8a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-06 11:59:46 +0200

    t7.14: Formatoi koodi

--- osa7/bloglist-frontend/src/App.tsx
+++ osa7/bloglist-frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import BlogList from "./components/BlogList";
 import CreateBlogForm from "./components/CreateBlogForm";
 import LoginForm from "./components/LoginForm";
 import Notification from "./components/Notification";
 import UserInfo from "./components/UserInfo";
+import UserTable from "./components/UserTable";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import { initializeBlogs } from "./reducers/blogReducer";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import UserTable from "./components/UserTable";
 import { initializeUsers } from "./reducers/userReducer";
 
 const App = () => {
